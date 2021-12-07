7b7a919fe4e4199f125a214e0be61867be2db888
lihaybs <lihaybs94@gmail.com>
2021-12-07 17:51:53 +0200

    Flip card with delay 1

--- memory-game.js
+++ memory-game.js
@@ -1,10 +1,56 @@
 const cards = [
-  { name: `A` },
-  { name: `B` },
-  { name: `C` },
-  { name: `A` },
-  { name: `B` },
-  { name: `C` },
+  { name: `🤬` },
+  { name: `😝` },
+  { name: `😜` },
+  { name: `☠️` },
+  { name: `💀` },
+  { name: `👺` },
+  { name: `👹` },
+  { name: `👿` },
+  { name: `😈` },
+  { name: `😨` },
+  { name: `😰` },
+  { name: `😡` },
+  { name: `🥵` },
+  { name: `😅` },
+  { name: `😂` },
+  { name: `🤣` },
+  { name: `🥲` },
+  { name: `☺️` },
+  { name: `😊` },
+  { name: `😟` },
+  { name: `😕` },
+  { name: `🙁` },
+  { name: `☹️` },
+  { name: `🥸` },
+  { name: `😎` },
+  { name: `🤓` },
+  { name: `🤬` },
+  { name: `😝` },
+  { name: `😜` },
+  { name: `☠️` },
+  { name: `💀` },
+  { name: `👺` },
+  { name: `👹` },
+  { name: `👿` },
+  { name: `😈` },
+  { name: `😨` },
+  { name: `😰` },
+  { name: `😡` },
+  { name: `🥵` },
+  { name: `😅` },
+  { name: `😂` },
+  { name: `🤣` },
+  { name: `🥲` },
+  { name: `☺️` },
+  { name: `😊` },
+  { name: `😟` },
+  { name: `😕` },
+  { name: `🙁` },
+  { name: `☹️` },
+  { name: `🥸` },
+  { name: `😎` },
+  { name: `🤓` },
 ];
 function randRange(a, b) {
   return Math.floor(a + Math.random() * (b - a));
@@ -28,17 +74,97 @@ function shuffle(arr) {
   }
 }
 function main() {
-    console.log([...cards]);
-    shuffle(cards)
-    console.log(cards);
+  console.log([...cards]);
+  shuffle(cards);
+  console.log(cards);
 }
 
-function chack(a,b) {
-    console.log(a);
-    console.log(b);
-  return(a.name==b.name && a!=b)
+function chack(a, b) {
+  console.log(a);
+  console.log(b);
+  return a.name == b.name && a != b;
 }
 
-console.log(chack(cards[randRange(0,cards.length)],cards[randRange(0,cards.length)]));
-main();
+// setTimeout (function(){
+//   alert(`Try again`);
+// }, 1000);
 
+// let chack = (arr,i,j) => i != j && arr[i].name === arr[j].name
+let i = 0;
+let compare = [];
+let compareF = (obj) => {
+  compare[i] = obj;
+  i++;
+  console.log(compare);
+
+  if (compare.length == 2) {
+    if (
+      !(
+        compare[0].innerHTML == compare[1].innerHTML &&
+        compare[0].id != compare[1].id
+      )
+    ) {
+      setTimeout(() => {
+        compare[0].innerHTML = ``;
+        compare[1].innerHTML = ``;
+      }, 2000);
+    }
+    setTimeout(() => {
+      compare = [];
+      i = 0;
+    }, 2200);
+  }
+};
+
+// if (i==2) {
+//   for(i of compare){
+//     console.log(i);
+//   }
+//   i=0
+//   compare[0] == compare[1] ? console.log(`You right`): console.log(`Try again`);
+//   compare=[];
+// }
+
+const cardClicked = (evn) => {
+  if (i != 2) {
+    const cardEl = evn.target;
+    const idx = cardEl.id;
+    // console.log(cards[idx]);
+    cardEl.innerHTML = cards[idx].name;
+    compareF(cardEl);
+  }
+};
+
+function creatCardElement(idx) {
+  const board = document.getElementById("board");
+  // console.log(board);
+  // document.querySelector("")
+  const cardEl = document.createElement("div");
+  // cardEl.innerHTML = card.name;
+  cardEl.id = idx;
+  cardEl.className = "card";
+  // cardEl.addEventListener(`click`, f);
+  cardEl.onclick = cardClicked;
+  // cardEl.onclick = ()=> {
+  //   alert(card.name);
+  // };
+
+  board.appendChild(cardEl);
+}
+// console.log(
+//   chack(cards[randRange(0, cards.length)], cards[randRange(0, cards.length)])
+// );
+function main() {
+  shuffle(cards);
+  for (let idx in cards) {
+    creatCardElement(idx);
+  }
+}
+
+const f = () => {
+  alert(`Hello`);
+};
+
+window.onload = () => {
+  main();
+};
